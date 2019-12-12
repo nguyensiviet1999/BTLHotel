fef4d1a00240400c7a05c3aa608429bbc0c6705b
Nguyễn Sỹ Việt <nguyensiviet1999@gmail.com>
2019-12-12 21:07:02 +0700

    submit

---
+++ BTLHotel/config/services.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+    ],
+
+    'postmark' => [
+        'token' => env('POSTMARK_TOKEN'),
+    ],
+
+    'ses' => [
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    ],
+    'google' => [
+        'client_id' => '692122075312-o0undop48v1prso5n8a2dvovek2nugd3.apps.googleusercontent.com',
+        'client_secret' => 'b_HHo_fI1X35pZ7aI8-t1I9A',
+        'redirect' => 'http://hotel-uet.herokuapp.com/auth/google/callback',
+    ],
+
+];
---
+++ BTLHotel/routes/web.php
@@ -0,0 +1,47 @@
+<?php
+//public route
+Route::get('/','publicPage@index');
+Route::get('/index','publicPage@index');
+Route::get('/about','publicPage@about');
+Route::get('/login','publicPage@login')->name('login');
+Route::get('/register','publicPage@register');
+Route::get('/blog','UserController@showBlog');
+Route::get('/room','publicPage@room');
+Route::get('/single_room/{id_room_type}','publicPage@singleRoom');
+
+
+//route admin
+Route::group(['prefix'=>'admin','middleware'=>'adminLogin'],function(){
+    Route::get('/quanly/{option?}/{id?}','adminController@quanly');
+        // function($option=null) {
+        // return view('admin.dashboard');
+    // });
+    // update_ad_password
+    Route::post('/quanly/update_ad_password','adminController@updateAdPassword')->name('update_ad_password');
+    Route::post('/quanly/insert_room_type','adminController@insertRoomType')->name('insert_room_type');
+    Route::post('/quanly/edit_room_type','adminController@editRoomType')->name('edit_room_type');
+    Route::post('/quanly/delete_room_type','adminController@editRoomType')->name('delete_room_type');
+    Route::post('/quanly/insert_room','adminController@insertRoom')->name('insert_room');
+    Route::post('/quanly/edit_room','adminController@editRoom')->name('edit_room');
+});
+//route user
+Route::group(['prefix'=>'user','middleware'=>'userLogin'],function(){
+    Route::get('/booking_form/{id_room_type?}','publicPage@booking');
+    Route::get('/profile','UserController@showProfileUser');
+    Route::get('/phong_da_book','UserController@showPhongDaBook');
+    Route::get('/delete_booked/{id}','UserController@deleteBooked')->name('delete_booked');
+    Route::post('/edit_profile_user','UserController@editProfileUser')->name('edit_profile_user');
+});
+//login hander,when login success, session setted with variable 'login'
+Route::post('dangnhap','UserController@login')->name('dangnhap');
+Route::post('register', 'publicPage@dangki');
+Route::post('booking', 'publicPage@postbooking')->name('booking');
+Route::post('feedback', 'publicPage@postFeedback')->name('feedback');
+Route::post('setpassword','UserController@setPassword')->name('setpassword');
+Route::put('setpassword','UserController@putPassword')->name('postpassword');
+Route::get('/auth/{provider}','SocialAuthController@redirectToProvider');
+Route::get('/auth/{provider}/callback','SocialAuthController@handleProviderCallback');
+Route::get('/qr-code','QrCodeController@getQrCode');
+Route::get('/return-vnpay','publicPage@returnVNPay')->name('return-vnpay');
+Route::post('creat_pay','publicPage@creatPay')->name('creat_pay');
+Route::post('suggest_room','publicPage@suggestRoom')->name('suggest_room');
